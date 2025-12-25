8ed391f6c863d40b6b33e75130a6bbe1241f7d48
hsn347 <hsn347@gmail.com>
2025-12-25 07:31:10 +0300

    delete

--- src/Routers/AppRoutes.tsx
+++ src/Routers/AppRoutes.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router';
 import { First_page } from '../pages/First_page';
-import { ChatPage } from '@/pages/ChatPage';
 
 export default function AppRoutes() {
   return (
